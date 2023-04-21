27a687a51c2ab471df9cea98b44f5aa09932acb2
Kathryn6368 <6368@holbertonstudents.com>
2023-04-20 18:53:38 -0700

    working properly

--- super_simple_shell.c
+++ super_simple_shell.c
@@ -11,16 +11,16 @@ int main(void)
 	int wait_return;
 	int i;
 	//char *argv[] = {"/bin/ls", "-l", "/usr/", NULL};
-	char *argv[] = {"filename", NULL};
+	char *argv[10];
 	size_t bytes;
 	char *buffer;
 	ssize_t got_line;
 
-	bytes = 0;
-	buffer = NULL;
-	got_line = 0;
 	while (1)
 	{
+		i = 0;
+		bytes = 0;
+		buffer = NULL;
 		printf("$ ");
 		got_line = getline(&buffer, &bytes, stdin);
 		if (buffer == NULL)
@@ -28,7 +28,23 @@ int main(void)
 			printf("Error: getline failed\n");
 			return (-1);
 		}
-		printf("Your command was: %s", buffer);
+		/* separate arguments*/
+		//printf("%s\n", buffer);
+		argv[i] = strtok(buffer, " \t\n");
+		while (argv[i] != NULL)
+		{
+			i = i + 1;
+			argv[i] = strtok(NULL, " \t\n");
+		}
+		argv[i] = 0;
+		i = 0;
+		/*print to TEST*/
+		while (argv[i] != NULL)
+		{
+			printf("argv[%d]: %s\n", i, argv[i]);
+			i = i + 1;
+		}
+		//printf("Your command was: %s", buffer);
 		child_pid = fork();
 		if (child_pid == -1)
 		{
@@ -37,9 +53,10 @@ int main(void)
 		}
 		if (child_pid == 0)
 		{
-			printf("I am child process, iteration number: %d\n", i);
-			argv[0] = buffer;
-			if (execve(buffer, argv, NULL) == -1)
+			//printf("I am child process, iteration number: %d\n", i);
+			//argv[0] = buffer;
+			//printf("%s\n", argv[0]);
+			if (execve(argv[0], argv, NULL) == -1)
 			{
 				perror("Error:");
 			}
@@ -49,9 +66,9 @@ int main(void)
 		else
 		{
 			wait_return = wait(&status);
-			printf("child process complete!\n");
+			printf("----------0-0-0-0-0-0-0-0-0-0-0-------------child process complete!\n");
 		}
-		printf("Program complete!\n");
+		//printf("Program complete!\n");
 		if (got_line == -1)
 		{
 			printf("exit\n");
